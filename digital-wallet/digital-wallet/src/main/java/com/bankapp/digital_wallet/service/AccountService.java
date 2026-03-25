a61804561f07d57cb49ccdedfa388060b3afd62f
amrutharakesh2000 <amrutharakesh2000@gmail.com>
2026-03-25 17:04:34 +0530

    Delete digital-wallet/digital-wallet directory
    deleted the dummy project dir

--- digital-wallet/digital-wallet/src/main/java/com/bankapp/digital_wallet/service/AccountService.java
+++
@@ -1,9 +0,0 @@
-package com.bankapp.digital_wallet.service;
-
-import com.bankapp.digital_wallet.entity.Account;
-import com.bankapp.digital_wallet.entity.User;
-
-public interface AccountService {
-     Account createAccount(User user);
-     Account getAccountByUser(User user);
-}
--- digital-wallet/digital-wallet/src/main/java/com/bankapp/digital_wallet/service/AuthService.java
+++
@@ -1,8 +0,0 @@
-package com.bankapp.digital_wallet.service;
-
-import com.bankapp.digital_wallet.dto.request.RegisterRequest;
-
-public interface AuthService {
-   String login(String email, String password);
-   String registerUser(RegisterRequest request);
-}
